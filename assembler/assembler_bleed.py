3e0f8e22b16bb22d16be2f067c9a8dd095ffed88
Simao-Leal <simao.frg.leal@gmail.com>
2024-12-01 21:19:30 +0000

    some minor changes before I never touch this again
    A SWERC foi bué fixe. Obrigado por tudo Lucas!

--- assembler/assembler_bleed.py
+++ assembler/assembler_bleed.py
@@ -1,43 +1,38 @@
 import glob
 import os
 
-offset = {
-    0: '-2.2mm 2.8mm', #top left
-    1: '2.2mm 2.8mm', #top right
-    2: '-2.2mm -2.8mm', #bottom left
-    3: '2.2mm -2.8mm', #bottom right
-}
-
-front_dir = '/Users/simaoleal/Desktop/swerc-print'
+front_dir = '/Users/simaoleal/Desktop/SWERC 24/front'
 #directory with front pdfs (one for each person)
 #we assume files are named '<role> <some number>.pdf' 
 #role should be lower case
 
-back_dir = '/Users/simaoleal/Desktop/swerc-print/back'
+back_dir = '/Users/simaoleal/Desktop/SWERC 24/back'
 #directory with back pdfs (one for each role)
 #we assume files are named '<role>.pdf'
 #role should be lower case
 
-output_dir = '/Users/simaoleal/Desktop/swerc-print/output'
+output_dir = '/Users/simaoleal/Desktop/SWERC 24/output'
 #where to put the final pdf files
 
-roles = ['contestant', 'coach', 'jury', 'organizer', 'volunteer', 'sponsor']
+roles = ['Volunteer'] #['Contestant', 'Coach', 'Jury', 'Organizer', 'Volunteer', 'Sponsor']
 
 positions = ['top-left', 'top-right', 'bottom-left', 'bottom-right']
 
 os.system(f"mkdir '{front_dir}/scaled'")
 os.system(f"mkdir '{back_dir}/scaled'")
 
+scale = 0.93
+
 for role in roles:
     #scaling backs
     for i, pos in enumerate(positions):
-        os.system(f"pdfjam --scale 0.962 --offset '{offset[i]}' --outfile \
+        os.system(f"pdfjam --scale {scale} --outfile \
                     '{back_dir}/scaled/{role}-{pos}.pdf' --a6paper -- '{back_dir}/{role}.pdf'")
 
-    merge_call = f"pdfjam --nup 2x2 --a4paper --outfile '{output_dir}/{role}-badges.pdf' -- "
+    merge_call = f"pdfjam --nup 2x2 --a4paper --outfile '{output_dir}/{role}-badges-bleed.pdf' -- "
     #scaling fronts
     for i, front_file in enumerate(glob.glob(f'{front_dir}/{role}*.pdf')):
-        os.system(f"pdfjam --scale 0.95 --outfile \
+        os.system(f"pdfjam --scale {scale} --outfile \
                   '{front_dir}/scaled/{role} {i}.pdf' --a6paper -- '{front_file}'")
         
         merge_call += f"'{front_dir}/scaled/{role} {i}.pdf' "
